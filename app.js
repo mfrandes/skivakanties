e8e48d67bb33a355db8c733beaf5df6bfbe47345
mfrandes <frandesmyrce@gmail.com>
2019-09-20 22:24:40 +0300

    donne

--- app.js
+++ app.js
@@ -19,8 +19,42 @@ var apos = require('apostrophe')({
 
     // If a template is not found somewhere else, serve it from the top-level
     // `views/` folder of the project
-
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
-
+    'apostrophe-attachments': {svgImages: true},
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+    'video-widgets': {},
+    'description-widgets': {},
+    'offert-widgets': {},
+    'details-widgets': {},
+    'footer-widgets': {},
+    'area-widgets' : {},
+    'products': {},
+    'products-widgets': {
+      extend: 'apostrophe-pieces-widgets',
+      filters: {
+        projection: {
+          title: 1,
+          name: 1,
+          location: 1,
+          city: 1,
+          address: 1,
+          country:1,
+          inDate:1,
+          outDate:1,
+          days:1,
+          price:1,
+          currency:1,
+          priceDetails:1,
+          select:1,
+          background:1,
+          starIcon:1,
+          locationPin:1,
+          calendar:1,
+          pretyTitle:1,
+          // Not a real database property, but including it in the projection
+          // fetches everything needed to populate it
+          _url: 1,
+        }
+      }
+    }
   }
 });
